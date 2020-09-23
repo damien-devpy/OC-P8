61e0b5bb2627f65aa4439a3b9737f0f18d1e8be5
Damien <damien.ramelet@protonmail.com>
2020-09-23 08:56:48 +0200

    Testing search_app context processor

--- purbeurre/search_app/forms.py
+++ purbeurre/search_app/forms.py
@@ -1,4 +1,8 @@
 from django import forms
 
+
 class SearchForm(forms.Form):
-    input_user = forms.CharField(label="Cherchez un aliment de substitution", max_length=100)
+    input_user = forms.CharField(label="Cherchez un aliment de substitution",
+                                 widget=forms.TextInput(attrs={
+                                     'placeholder': 'Recherchez un produit'
+                                 }))
---
+++ purbeurre/search_app/tests/unit/test_context_processor.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from search_app.context_processors import search_form
+from search_app.forms import SearchForm
+
+
+class ContextProcessorTest(TestCase):
+    """Test search_app template context processor displaying a search form in
+    base.html.
+    """
+    def setUp(self):
+        """Setting up a context object. Passing an object as request."""
+        requestObject = None
+        self.context = search_form(requestObject)
+
+    def test_search_form_return_a_dict(self):
+        self.assertEqual(type(self.context), dict)
+
+    def test_search_form_return_contain_correct_keys(self):
+        for key_context, key_expect in zip(self.context.keys(),
+                                           ['search_form']):
+            self.assertEqual(key_context, key_expect)
+
+    def test_search_form_return_contain_correct_values(self):
+        for an_object, a_class in zip(self.context.values(), (SearchForm,)):
+            self.assertIsInstance(an_object, a_class)
