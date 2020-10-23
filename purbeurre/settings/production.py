cc0c47e807dee6d290c65d75b512c4fb2847e0d4
Damien <damien.ramelet@protonmail.com>
2020-10-23 11:57:58 +0200

    Production settings

--- purbeurre/settings/production.py
+++ purbeurre/settings/production.py
@@ -1,6 +1,5 @@
 from .base import *
-import dj_database_url
-import django_heroku
+from os import environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -9,7 +8,12 @@ DEBUG = False
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'purbeurre_db',
+        'USER': 'purbeurre_db_admin',
+        'PASSWORD': environ.get('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
-
-django_heroku.settings(locals())
